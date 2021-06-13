3566ece16aa6fa1776615889fd60cc0ad02ac20d
Sandesh Raj Poudel <sandeshrajpoudel1@gmail.com>
2021-06-13 10:40:46 +0545

    finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 //import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
+import {SearchBox} from './components/search-box/search-box.component'
+
 import {CardList} from './components/card-list/card-list.component'
 
 class App extends Component {
@@ -22,6 +24,14 @@ class App extends Component {
   render(){
   return (
     <div className="App">
+      <SearchBox
+      placeholder='search monsters'
+    //   handleChange={e=> this.setState({
+    //     searchField: e.target.value
+
+    //   })
+    // }
+      />
     <CardList monsters={this.state.monsters}>
      
   
